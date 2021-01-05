33a554e72d1a14467191195c4d1ce73477572f0a
Shyzette <72762164+Shyzette@users.noreply.github.com>
2021-01-05 11:01:37 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 while True:
     print("light level:" + input.light_level())
     if input.light_level() > 15:
-        light.clear()
-    elif input.light_level() > 10: 
-        light.set_all(light.rgb(0,0,255))
+        light.clear()  
     else: 
-        light.set_all(light.rgb(255,255,0))
+        light.set_all(light.rgb(0,0,255))
+ 
--- main.ts
+++ main.ts
@@ -2,10 +2,8 @@ while (true) {
     console.log("light level:" + input.lightLevel())
     if (input.lightLevel() > 15) {
         light.clear()
-    } else if (input.lightLevel() > 10) {
-        light.setAll(light.rgb(0, 0, 255))
     } else {
-        light.setAll(light.rgb(255, 255, 0))
+        light.setAll(light.rgb(0, 0, 255))
     }
     
 }
